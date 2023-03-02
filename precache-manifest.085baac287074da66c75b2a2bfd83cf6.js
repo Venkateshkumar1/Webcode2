9ed559d092f59f81af78b153f29e20191d29cde0
Venkateshkumar1 <kishorkani4321@gmail.com>
2023-03-02 13:02:08 +0530

    Updates

---
+++ precache-manifest.085baac287074da66c75b2a2bfd83cf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b4df94d6eddb0ee1ca52cd58d4cd54",
+    "url": "/Webcode2-frontend/index.html"
+  },
+  {
+    "revision": "5c4aa7908743baef9bc3",
+    "url": "/Webcode2-frontend/static/css/main.65013ead.chunk.css"
+  },
+  {
+    "revision": "4e68744c6c359b2970c7",
+    "url": "/Webcode2-frontend/static/js/2.889da77e.chunk.js"
+  },
+  {
+    "revision": "5c4aa7908743baef9bc3",
+    "url": "/Webcode2-frontend/static/js/main.85f0b677.chunk.js"
+  },
+  {
+    "revision": "7e926e452826ef66cbb5",
+    "url": "/Webcode2-frontend/static/js/runtime-main.e043ab1a.js"
+  }
+]);
